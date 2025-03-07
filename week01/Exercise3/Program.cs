e9d44055177adac124553ba8b654c1a889045790
Codecamp-champ <aaron92coker@gmail.com>
2025-03-07 02:04:58 -0700

    Core and stretch challenge completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101); // Generates a random number between 1 and 100
+            int guess = 0;
+            int guessCount = 0;
+
+            Console.WriteLine("I am thinking of a number between 1 and 100. Try to guess it!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    guessCount++;
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        //Stretch challenge
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"It took you {guessCount} guesses.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainInput = Console.ReadLine().ToLower();
+            playAgain = playAgainInput == "yes";
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
